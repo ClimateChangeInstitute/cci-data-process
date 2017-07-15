5e7aab8434751588dee7c1f4632873b6cc8a997d
markroyer <mark.e.royer@gmail.com>
2017-07-15 09:11:19 -0400

    Add some example tests for findMean

--- test/climatechange/test_resampleStats.py
+++ test/climatechange/test_resampleStats.py
@@ -11,10 +11,21 @@ from climatechange.resampleStats import findMin
 from climatechange.resampleStats import findStd
 from climatechange.resampleStats import findPtsYear
 from climatechange.resampleStats import compileStats
+from math import nan
 
-array = [[5, 3, 4, 5, 3]]
-array1=[[]]
-array2=[[5, 3, 4, 5, 3],[1, 2, 3, 4],[4, 4]]
+emptyArray = []
+singleRowArray = [[5, 3, 4, 5, 3]]
+multipleRowArray = [[5.0, 4.0, 3.0, 2.0, 1.0],
+                   [2.0, 3.0, 4.0, 5.0, 6.0 ],
+                   [1.0, 3.0, 2.0, 5.0, 4.0]]
+containingNoneArray = [[5.0, None, None, 2.0, 1.0],
+                       [2.0, None, None, 5.0, 6.0 ],
+                       [1.0, None, None, 5.0, 4.0]]
+containingNaNArray = [[5.0, nan, nan, 2.0, 1.0],
+                       [2.0, nan, nan, 5.0, 6.0 ],
+                       [1.0, nan, nan, 5.0, 4.0]]
+
+
 
 class Test(unittest.TestCase):
 
@@ -31,41 +42,43 @@ class Test(unittest.TestCase):
         pass
 
 # test for all functions with same name
-    def testfindMean(self):       
-        result = findMean(array)
-        self.assertAlmostEqual([4], result)     
-        pass
+    def testfindMean(self):
+        self.assertAlmostEqual([], findMean(emptyArray))
+        self.assertAlmostEqual([4.0], findMean(singleRowArray))
+        self.assertAlmostEqual([3.0, 4.0, 3.0], findMean(multipleRowArray))
+        
+        # Maybe throw an error?
+        # self.assertAlmostEqual([None, None, None], findMean(containingNoneArray))
+        
+        # Similarly what to do with nan values?
+        # self.assertAlmostEqual([nan, nan, nan], findMean(containingNaNArray))
+    
+        # Also assume that 2D array is rectangular shape? No funny business. :-)
     
     def testfindMedian(self):       
-        result=findMedian(array)
-        self.assertAlmostEqual([4], result)        
-        pass
+        result = findMedian(singleRowArray)
+        self.assertAlmostEqual([4], result)
     
     def testfindMax(self):
-        result=findMax(array)
-        self.assertAlmostEqual([5],result)
-        pass
+        result = findMax(singleRowArray)
+        self.assertAlmostEqual([5], result)
     
     def testfindMin(self):    
-        result=findMin(array)
-        self.assertAlmostEqual([3],result)        
-        pass
+        result = findMin(singleRowArray)
+        self.assertAlmostEqual([3], result)
     
     def testfindStd(self):
-        result=findStd(array)
-        self.assertAlmostEqual([0.894427190999915860],result)
-        pass
+        result = findStd(singleRowArray)
+        self.assertAlmostEqual([0.894427190999915860], result)
     
     def testfindPtsYear(self):
-        result=findPtsYear(array)
-        self.assertAlmostEqual([5],result)
-        pass
+        result = findPtsYear(singleRowArray)
+        self.assertAlmostEqual([5], result)
         
     def testcompileStats(self):
-        result=compileStats(array)
-        self.assertAlmostEqual([[4],[4], [5], [3], [0.89442719099991586], [5]], result)
-        pass
+        result = compileStats(singleRowArray)
+        self.assertAlmostEqual([[4], [4], [5], [3], [0.89442719099991586], [5]], result)
         
 if __name__ == "__main__":
-    #import sys;sys.argv = ['', 'Test.testName']
+    # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
