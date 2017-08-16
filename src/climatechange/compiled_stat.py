47646e73984ea7f481576059fa6b9deb04597299
markroyer <mark.e.royer@gmail.com>
2017-08-16 12:26:52 -0400

    Convert windows to unix style files

--- src/climatechange/compiled_stat.py
+++ src/climatechange/compiled_stat.py
@@ -8,22 +8,33 @@ from climatechange.headers import Header
 
 class CompiledStat(object):
     '''
-    object that contains all of the properties with statistics compiled
+    Contains a :py:class:`pandas.core.frame.DataFrame` that contains **Mean**,
+    **Stdv**, **Median**, **Max**, **Min**, and **Count**.
+    '''
+
+    '''
+    :ivar df: Contains columns [Mean,Stdv, Median, Max, Min, and Count]
     '''
     df:DataFrame
-    
+
+    '''
+    :ivar x_header: The header information for the index column
+    '''
     x_header:Header
-    
+
+    '''
+    :ivar sample_header: The header information for the compiled statistics
+    '''
     sample_header:Header
-    
+
 
     def __init__(self, df:DataFrame,x_header:Header,sample_header:Header):
         '''
-        Constructor
+
+        :param df: Contains columns Mean,Stdv, Median, Max, Min, and Count
+        :param x_header: The index column
+        :param sample_header: The header the statistics are based on
         '''
         self.df=df
         self.x_header=x_header
         self.sample_header=sample_header
-        
-        
-        
--- src/climatechange/resample_data_by_depths.py
+++ src/climatechange/resample_data_by_depths.py
@@ -16,12 +16,12 @@ from pandas import Series
 
 def create_range_for_depths(list_to_inc:List[float], inc_amt: int=0.01) -> List[float]:
     '''
-    
+
     :param list_to_inc:
     :param inc_amt:
     '''
     if str(min(list_to_inc))[::-1].find('.') > str(inc_amt)[::-1].find('.'):
-        
+
         r = str(min(list_to_inc))[::-1].find('.') - 1
     else:
         r = str(inc_amt)[::-1].find('.')
@@ -30,7 +30,7 @@ def create_range_for_depths(list_to_inc:List[float], inc_amt: int=0.01) -> List[
 
 def find_index_by_increment_for_depths(list_to_inc:List[float], inc_amt:int=0.01) -> List[List[float]]:
     '''
-    
+
     :param list_to_inc:
     :param inc_amt:
     '''
@@ -43,7 +43,7 @@ def resampled_depths(df_x_sample:DataFrame, depth_header:Header, inc_amt):
     bottom_range = [x + inc_amt for x in top_range]
 #     if len(index_to_remove)>0:
 #         for i in sorted(index_to_remove, reverse=True):
-# 
+#
 #             del top_range[i]
 #             del bottom_range[i]
     df = DataFrame([top_range, bottom_range]).transpose()
@@ -65,58 +65,58 @@ def resampled_by_inc_depths(df_x_sample:DataFrame,
     :param df:
     :param inc:
     '''
-   
+
 #     print(index)
 #     index_to_remove=[index.index(i) for i in index if len(i)<1 or i[0]==0]
 #     index = [i for i in index if len(i)>1]
 #     print(index)
 #     print(index_to_remove)
 
-            
+
     df_depths = resampled_depths(df_x_sample, depth_header, inc_amt)
-#     df_depths = resampled_depths(df_x_sample, depth_header, inc_amt,index_to_remove)   
+#     df_depths = resampled_depths(df_x_sample, depth_header, inc_amt,index_to_remove)
     df_stats = resampled_statistics_by_x(df_x_sample, inc_amt)
     return pandas.concat([df_depths, df_stats], axis=1)
 
 def compile_stats_by_depth(df:DataFrame, depth_header:Header, sample_header:Header, inc_amt:float) -> CompiledStat:
     '''
-    From the given data frame compile statistics (mean, median, min, max, etc) 
+    From the given data frame compile statistics (mean, median, min, max, etc)
     based on the parameters.
-    
+
     :param df: The data to compile stats for
     :param depth_header: The depth column to use for indexing
     :param sample_header: The sample compile to create statistics about
-    :param inc_amt: The amount to group the year column by. For example, 
+    :param inc_amt: The amount to group the year column by. For example,
     2012.6, 2012.4, 2012.2 would all be grouped into the year 2012.
-    :return: A new DataFrame containing the resampled statistics for the 
+    :return: A new DataFrame containing the resampled statistics for the
     specified sample and year.
     '''
     df_x_sample = pandas.concat([df.loc[:, depth_header.name], df.loc[:, sample_header.name]], axis=1)
     resampled_data = resampled_by_inc_depths(df_x_sample, depth_header, inc_amt)
-    
+
     return CompiledStat(resampled_data, depth_header, sample_header)
 
-         
-def find_index_of_depth_intervals(depth_large:Series,depth_small:Series)->List[List[int]]:    
+
+def find_index_of_depth_intervals(depth_large:Series,depth_small:Series)->List[List[int]]:
     '''
-    
+
     :param depth_large:depth columns of larger dataset with smaller increment
     :param depth_small:depth columns of smaller dataset with larger increment
     '''
     bottom_depth=[]
     for i in range(depth_small.size-1):
         bottom_depth.append(depth_small.loc[i+1])
-    index=[find_indices(depth_large, lambda e: e >= depth_small[i] and e < depth_small[i+1]) for i in range(0, len(depth_small)-1)] 
+    index=[find_indices(depth_large, lambda e: e >= depth_small[i] and e < depth_small[i+1]) for i in range(0, len(depth_small)-1)]
     return index
- 
+
 def compiled_stats_by_dd_intervals(larger_df:DataFrame,smaller_df:DataFrame) -> List[List[CompiledStat]]:
     '''
-    From the given data frame compile statistics (mean, median, min, max, etc) 
+    From the given data frame compile statistics (mean, median, min, max, etc)
     based on the parameters.
-     
+
     :param df1:Larger Dataframe with smaller intervals to create a compiled stat
     :param df2:Smaller Dataframe with larger intervals to create index of intervals
-    :return: A list of list of CompiledStat containing the resampled statistics for the 
+    :return: A list of list of CompiledStat containing the resampled statistics for the
     specified sample and depth by the depth interval from df2.
     '''
     headers_larger=process_header_data(larger_df)
@@ -124,11 +124,11 @@ def compiled_stats_by_dd_intervals(larger_df:DataFrame,smaller_df:DataFrame) -> 
     depth_headers_larger=[h for h in headers_larger if h.htype == HeaderType.DEPTH]
     depth_headers_smaller=[h for h in headers_smaller if h.htype == HeaderType.DEPTH]
     sample_headers_larger=[h for h in headers_larger if h.htype == HeaderType.SAMPLE]
-     
+
     result_list = []
     for depth_header_l in depth_headers_larger:
         for depth_header_s in depth_headers_smaller:
-            if depth_header_l==depth_header_s:    
+            if depth_header_l==depth_header_s:
                 index=find_index_of_depth_intervals(larger_df.loc[:,depth_header_s.name],smaller_df.loc[:,depth_header_l.name])
                 depth_df=create_top_bottom_depth_dataframe(smaller_df,depth_header_s)
                 depth_samples=[]
@@ -143,21 +143,19 @@ def compiled_stats_by_dd_intervals(larger_df:DataFrame,smaller_df:DataFrame) -> 
     return result_list
 
 def create_top_bottom_depth_dataframe(df:DataFrame,depth_header:Header)->DataFrame:
-    
+
     new_depth_headers=create_depth_headers([depth_header])
-    
+
     list_depth = []
     for i in range(0, df.shape[0]-1):
         list_depth.append([df.loc[i,depth_header.name],df.loc[i+1,depth_header.name]])
     return DataFrame(list_depth,columns=new_depth_headers)
 
 def create_top_bottom_depth_dataframe_for_laser_file(df:DataFrame,depth_header:Header)->DataFrame:
-    
+
     new_depth_headers=create_depth_headers([depth_header])
-    
+
     list_depth = []
     for i in range(0, df.shape[0]-1):
         list_depth.append([df.loc[i,depth_header.name],df.loc[i+1,depth_header.name]])
     return DataFrame(list_depth,columns=new_depth_headers)
-
-
